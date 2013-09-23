19d8e4f5e793db052ae7c0adecffd2fff770094e
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-09-23 11:02:05 +0200

    Add sysmon & php-daemon

--- recipes/php.rb
+++ recipes/php.rb
@@ -1,7 +1,24 @@
-package "newrelic-php5" do
-    action :install
+if node['newrelic']['server_monitoring']['license'] != "" then
+    package "newrelic-sysmond"
+
+    template "#{node['php']['ext_conf_dir']}/newrelic.ini" do
+      source 'newrelic.ini.php.erb'
+      owner 'root'
+      mode '0640'
+      notifies :restart, "service[newrelic-daemon]"
+      notifies :restart, "service[php-fpm]"
+    end
+
+    package "newrelic-php5" do
+        action :install
+    end
+
+    service "newrelic-daemon" do
+        action [ :enable, :start, :restart ]
+    end
+
+    service "php5-fpm" do
+        action [ :enable, :start, :restart ]
+    end
 end
 
-service "newrelic-php5" do
-      action [ :enable, :start, :restart ]
-end
--- recipes/sysmond.rb
+++ recipes/sysmond.rb
@@ -5,10 +5,14 @@ if node['newrelic']['server_monitoring']['license'] != "" then
       source 'nrsysmond.cfg.erb'
       owner 'root'
       mode '0640'
+      notifies :restart, "service[newrelic-daemon]"
       notifies :restart, "service[newrelic-sysmond]"
     end
 
     service "newrelic-sysmond" do
       action [ :enable, :start, :restart ]
+    end
+    service "newrelic-daemon" do
+          action [ :enable, :start, :restart ]
     end
 end
